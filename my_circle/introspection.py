00f7a0bd7e7f32becf2a6c8d31e39188366fcee3
Дмитрий Соловьёв <sdmitriyyu@gmail.com>
2024-04-23 19:51:00 +0300

    интроспекция

--- my_circle/introspection.py
+++
@@ -1,8 +0,0 @@
-class introspection_info:
-    def __init__(self, obj):
-        self.obj = obj
-
-
-introspection = introspection_info(23)
-
-print(type(introspection), dir(introspection), isinstance(introspection, introspection_info))
